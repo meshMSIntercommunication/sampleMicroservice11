4f8f7659d633e0fb6ffaa6e7e8b2aebd00f2059f
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-10-24 09:03:40 -0500

    Add files via upload

---
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -0,0 +1,13 @@
+package com.co.services.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@RestController
+public class SampleController {
+	@GetMapping("/")
+	public String sampleApi() {
+		return "Sample Microservice 10 " + new Date().getTime();
+	}
+}
